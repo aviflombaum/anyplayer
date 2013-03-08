8bd6d2e086da2f1e553d5d26e27d3497bca82cab
Sunny Ripert <sunny@sunfox.org>
2013-03-08 13:03:05 +0100

    Tests on bin/anyplayer

---
+++ test/bin_test.rb
@@ -0,0 +1,135 @@
+require "test_helper"
+
+class AnyplayerTest < MiniTest::Unit::TestCase
+  def test_usage
+    usage = <<USAGE
+Usage: #{$0} [-v] [command]
+
+Where command is one of: playpause, play, pause, next, prev, voldown, volup,
+volume, track, artist, album, vote, name, launched.
+
+Player connected: Noplayer.
+USAGE
+
+    assert_output usage do
+      assert_exists_with_status(1) do
+        load 'bin/anyplayer'
+      end
+    end
+
+    with_argv %w(foobar) do
+      assert_output usage do
+        assert_exists_with_status(1) do
+          load 'bin/anyplayer'
+        end
+      end
+    end
+  end
+
+  def test_artist
+    assert_bin_anyplayer(argv: %w(artist), output: "The Drums\n")
+  end
+
+  def test_artist_verbose
+    assert_bin_anyplayer(argv: %w(-v artist), output: "The Drums\n",
+      error_output: "Loaded noplayer\nNoplayer launched?\n")
+  end
+
+  def test_playpause
+    assert_bin_anyplayer(argv: %w(playpause))
+  end
+
+  def test_play
+    assert_bin_anyplayer(argv: %w(play))
+  end
+
+  def test_pause
+    assert_bin_anyplayer(argv: %w(pause))
+  end
+
+  def test_next
+    assert_bin_anyplayer(argv: %w(next))
+  end
+
+  def test_prev
+    assert_bin_anyplayer(argv: %w(prev))
+  end
+
+  def test_voldown
+    assert_bin_anyplayer(argv: %w(voldown))
+  end
+
+  def test_volup
+    assert_bin_anyplayer(argv: %w(volup))
+  end
+
+  def test_volume
+    assert_bin_anyplayer(argv: %w(volume), output: "100\n")
+  end
+
+  def test_track
+    assert_bin_anyplayer(argv: %w(track), output: "Money\n")
+  end
+
+  def test_artist
+    assert_bin_anyplayer(argv: %w(artist), output: "The Drums\n")
+  end
+
+  def test_album
+    assert_bin_anyplayer(argv: %w(album), output: "Money\n")
+  end
+
+  def test_vote
+    assert_bin_anyplayer(argv: %w(vote))
+  end
+
+  def test_name
+    assert_bin_anyplayer(argv: %w(name), output: "Noplayer\n")
+  end
+
+  def test_launched
+    assert_bin_anyplayer(argv: %w(launched))
+  end
+
+  def test_playing
+    assert_bin_anyplayer(argv: %w(playing))
+  end
+
+  def test_paused
+    assert_bin_anyplayer(argv: %w(paused), exit: 1)
+  end
+
+  private
+
+    def assert_bin_anyplayer(argv: nil, exit: 0, output: "", error_output: "")
+      assert_exists_with_status(exit) do
+        assert_output output, error_output do
+          with_argv argv do
+            load 'bin/anyplayer'
+            yield if block_given?
+          end
+        end
+      end
+    end
+
+    def assert_exists_with_status(status)
+      begin
+        yield
+      rescue SystemExit => e
+        assert_equal status, e.status, "SystemExit with status expected"
+        return
+      end
+
+      flunk "SystemExit expected to be raised." if status != 0
+    end
+
+    # Replace ARGV temporarily in the given block
+    def with_argv(argv)
+      original_argv = ARGV.dup
+      ARGV.replace argv
+      yield
+    ensure
+      ARGV.replace original_argv
+    end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
+# Test libraries
 require "minitest/unit"
 require "minitest/autorun"
 require "flexmock/test_unit"
+
+
+# Anyplayer with the testing noplayer first
 require "anyplayer"
 
 require "anyplayer/players/noplayer"
